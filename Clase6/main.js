c6425526b22f9fb8c0a915d09ab6c73780bc1ec7
Irene Sanchez <iirene@Irenes-Personal.local>
2026-03-04 16:42:59 -0600

    Anadir archivos de clase 6

---
+++ Clase6/main.js
@@ -0,0 +1,47 @@
+const age = 33;
+var number = 10;
+
+var numberFloat = 15.60;
+// console.log(numberFloat + 10); // Suma el valor
+
+let numberWithZero = '15.60';
+// console.log(numberWithZero + 10); // Concatena el texto
+
+true
+false
+
+// Numeros +, -, *, /, >, <
+
+const suma = 3 * ((40 + 20) * 2);
+// suma = 5;
+// console.log(suma);
+
+// Strings 
+let firstName = 'Irene';
+const lastName = "Sanchez";
+
+// console.log(firstName + lastName);
+// console.error('ESTO ES UN ERRROR!!');
+
+// Booleans
+true 
+false
+
+const num = 10;
+
+// && -> and -> y
+
+console.log(num == 10 && num < 2);
+
+// || -> or -> o
+
+console.log(num == 10 || num < 2);
+
+
+
+
+
+
+
+
+
